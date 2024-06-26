1381cb021a15a6036e051dda9aafae872a0f18ab
joaovictor001 <joaocarvalhoborges@outlook.com>
2024-06-26 14:16:40 -0300

    feat: Create methods for Users

--- src/main/java/com/picpaybackend/picpaybackend/Services/UserService.java
+++ src/main/java/com/picpaybackend/picpaybackend/Services/UserService.java
@@ -1,11 +1,13 @@
 package com.picpaybackend.picpaybackend.Services;
 
 import com.picpaybackend.picpaybackend.Entities.User;
+import com.picpaybackend.picpaybackend.dtos.User.UserCreateDTO;
 import com.picpaybackend.picpaybackend.enums.UserType;
 import com.picpaybackend.picpaybackend.repository.UserReposity;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class UserService {
     @Autowired
@@ -22,6 +24,18 @@ public class UserService {
     }
     public User findUserById(long Id ) throws Exception {
         return this.reposity.findUserById(Id).orElseThrow(()-> new Exception("usurio not found"));
+    }
+
+
+    public User createUser(UserCreateDTO dados ){
+        User newUser = new User(dados);
+        this.saveUser(newUser);
+        return  newUser;
+
+    }
+
+    public List<User> getAllUsers(){
+        this.reposity.findAll();
     }
     public void saveUser(User user){
         this.reposity.save(user);
